ee161a5c4986ac3be1b415874b866925a9766264
yuwei <178967037@qq.com>
2018-09-07 16:34:03 +0800

    【新增】 库存查询

--- src/routes/stock/index.js
+++ src/routes/stock/index.js
@@ -1,9 +1,77 @@
 import React, { PureComponent } from 'react';
-import { Button } from 'antd-mobile'
+import { NavBar, Icon , SearchBar , Tabs } from 'antd-mobile';
+import { StickyContainer, Sticky } from 'react-sticky';
+import ListViewScroll from '../../components/listViewScroll';
+function renderTabBar(props) {
+  return (<Sticky>
+    {({ style }) => <div style={{ ...style, zIndex: 1 }}><Tabs.DefaultTabBar {...props} /></div>}
+  </Sticky>);
+}
+const tabs = [
+  { title: '全部' },
+  { title: '更多筛选' }
+];
+
 class Stock extends PureComponent {
+
+  state = {
+    url: '',
+    searchName: '',
+  }
+
   render() {
+    const { url , searchName } = this.state;
     return (
-      <Button type='primary'>库存查询</Button>
+      <div>
+        {/* 导航栏 */}
+        <NavBar
+          mode="light"
+          leftContent={[
+            <Icon key="0" type="left" style={{ marginRight: '10px' }} />,
+            <span key="1">返回</span>,
+          ]}
+          onLeftClick={() => console.log('onLeftClick')}
+          >库存查询</NavBar>
+
+        {/* 搜索栏 */}
+        <SearchBar
+          placeholder="通用名/商品名/生产厂家"
+          onSubmit={value => console.log(value, 'onSubmit')}
+          onClear={value => console.log(value, 'onClear')}
+          onFocus={() => console.log('onFocus')}
+          onBlur={() => console.log('onBlur')}
+          onCancel={() => console.log('onCancel')}
+          onChange={this.onChange}
+        />
+        {/* tab页切栏 */}
+        <StickyContainer>
+          <Tabs 
+            tabs={tabs}
+            initalPage={'t1'}
+            renderTabBar={renderTabBar}
+            >
+              <div style={{ display: 'flex', height:'70vh' , backgroundColor: '#fff' }}>
+                <ul>
+                  <ListViewScroll 
+                    url={`https://www.easy-mock.com/mock/5b8d3b510ab8991436ebd336/spd/acceptanceList`}
+                    queryParams={{
+                      searchParam: searchName
+                    }}
+                    method="GET"
+                    item={item => {
+                      console.log(item)
+                      return <li key={item.id}>{item.supplierName}</li>
+                    }}
+                  />
+                </ul>
+              </div>
+              <div style={{ display: 'flex' , height:'70vh' , backgroundColor: '#fff' }}>
+                Content of second tab
+              </div>
+          </Tabs>
+        </StickyContainer>
+
+      </div>
     )
   }
 }

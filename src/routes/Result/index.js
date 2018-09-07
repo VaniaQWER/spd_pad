5f880a49d58d71ee545349343cc1b566f7bc60ea
keshelou0035 <2994707742@qq.com>
2018-09-07 15:30:24 +0800

    basicLayout css等
    update

---
+++ src/routes/Result/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: gaofengjiao 
+ * @Date: 2018-08-20 10:44:41 
+ * @Last Modified by: gaofengjiao
+ * @Last Modified time: 2018-08-24 15:08:25
+ * 建设中页面
+ */
+import React, { PureComponent} from 'react';
+import styles from './result.css';
+alert(1234)
+class ResultInfo extends PureComponent{
+  render(){
+    return(
+      <div>
+        <img src={require("../../assets/image/construction.svg")}  alt="正在建设中" className={styles.imgInfo}/>
+        <h3 className={styles.title}>页面正在建设中...</h3>
+      </div>
+    )
+  }
+}
+
+export default ResultInfo;
--- src/routes/acceptance/index.js
+++ src/routes/acceptance/index.js
@@ -1,9 +1,21 @@
 import React, { PureComponent } from 'react';
-
+import { NavBar, Icon } from 'antd-mobile';
 class Acceptance extends PureComponent {
   render() {
     return (
-      <div>验收</div>
+      <div>
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left" />}
+          leftContent="Back"
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+            <Icon key="1" type="ellipsis" />,
+          ]}
+        >
+          验收
+        </NavBar>
+      </div>
     )
   }
 }
